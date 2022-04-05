1707c6ab0699809cb6f7768cfe98e2309d61ef87
Kajeloki <101186819+Kajeloki@users.noreply.github.com>
2022-04-05 11:41:13 +0000

    1

--- Main.java
+++ Main.java
@@ -5,13 +5,18 @@ class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
     char znak;
+    double rand=Math.random();
     //System.out.println(znak);
   //  char[] slowo={'A','d','a','m'};
    double tablica[]=new double[10];
     for(int i=0;i<10;i++)
       {
-        System.out.println("Podaj kolejna liczbe: ");
-    tablica[i]=myObj.nextDouble();
+       // System.out.println("Podaj kolejna liczbe: ");
+    tablica[i]=Math.random()*100;
+      }
+    for(int i=0;i<10;i++)
+      {
+        System.out.println(tablica[i]);
       }
     double suma=0;
       for(int i=0;i<10;i++)
